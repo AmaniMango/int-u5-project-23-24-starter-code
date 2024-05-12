5d0114a4c6a2265515b45329599f6286baa62a79
amanicps <80164449+AmaniMango@users.noreply.github.com>
2024-05-12 22:47:37 +0000

    done th js

--- script.js
+++ script.js
@@ -5,15 +5,17 @@ let results = document.querySelector(".results");
 let boredContainer = document.querySelector("#imageContainer");
 
 let boredUrls = ["sushi1.jpg", "pizzaa.jpg", "ice.jpg", "ramen.jpg"];
+let happyUrls = ["beef.jpg", "cupcakeee.jpg", "donuttt.jpg", "Spaghtettiii.jpg"];
+let anxiousUrls = ["elotes.jpg", "steak.jpg", "taco.jpg", "chil.jpg"];
+let sadUrls = ["pancake.jpg", "Waffle.jpg", "eggs.jpg", "cro.jpg"];
 
 Button.addEventListener("click", function() {
    let answer1 = input.value;
-
-
+   
     if (answer1 === "bored"){
         console.log("BORED");
         results.innerHTML = `<div class = "food1">
-        <h1>Sushi</h1>`;
+        <h1>Food to eat when you are bored</h1>`;
 
         boredUrls.forEach(function(url){
             let image = document.createElement("img");
@@ -25,17 +27,41 @@ Button.addEventListener("click", function() {
     else if (answer1 === "happy"){
         console.log("HAPPY");
         results.innerHTML = `<div class = "food2">
-        <h1>Pizza</h1>
-        <img src = "pizzaa.jpg">`;
+        <h1>Food to eat when you are happy</h1>`;
+
+        happyUrls.forEach(function(url){
+            let image = document.createElement("img");
+            image.src = url;
+            imageContainer.appendChild(image);
+
+        });
     }
     
-    // else if (answer1 === "anxious"){
+    else if (answer1 === "anxious"){
+        console.log("ANXIOUS");
+        results.innerHTML = `<div class = "food3">
+        <h1>Food to eat when you are anxious</h1>`;
+
+        anxiousUrls.forEach(function(url){
+            let image = document.createElement("img");
+            image.src = url;
+            imageContainer.appendChild(image);
+
+        });
+    }
     
-    // }
-    
-    // else if (answer1 === "sad"){
-    
-    // }
+    else if (answer1 === "sad"){
+        console.log("SAD");
+        results.innerHTML = `<div class = "food4">
+        <h1>Food to eat when you are sad</h1>`;
+
+        sadUrls.forEach(function(url){
+            let image = document.createElement("img");
+            image.src = url;
+            imageContainer.appendChild(image);
+
+        });
+    }
 
 });
 
