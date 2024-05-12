32dd760683f76928c8c447e0a87f2c164632eb59
amanicps <80164449+AmaniMango@users.noreply.github.com>
2024-05-12 19:51:04 +0000

    bored done - sushi, pizza, icecream, ramen

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-// Declare your arrays here
 
-// Make sure to declare your HTML elements as variables! 
+let input = document.querySelector(".q");
+let Button = document.querySelector("button");
+let results = document.querySelector(".results");
+let boredContainer = document.querySelector("#imageContainer");
 
-// Submit Button 
-let submitButton = document.querySelector("button");
-submitButton.addEventListener("click", function() {});
+let boredUrls = ["sushi1.jpg", "pizzaa.jpg", "ice.jpg", "ramen.jpg"];
+
+Button.addEventListener("click", function() {
+   let answer1 = input.value;
+
+
+    if (answer1 === "bored"){
+        console.log("BORED");
+        results.innerHTML = `<div class = "food1">
+        <h1>Sushi</h1>`;
+
+        boredUrls.forEach(function(url){
+            let image = document.createElement("img");
+            image.src = url;
+            imageContainer.appendChild(image);
+        });
+    }
+    
+    else if (answer1 === "happy"){
+        console.log("HAPPY");
+        results.innerHTML = `<div class = "food2">
+        <h1>Pizza</h1>
+        <img src = "pizzaa.jpg">`;
+    }
+    
+    // else if (answer1 === "anxious"){
+    
+    // }
+    
+    // else if (answer1 === "sad"){
+    
+    // }
+
+});
+
+
+
